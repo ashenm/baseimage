92ff57ff6b2cc5caf62106e495af87056230c17a
Ashen Gunaratne <mail@ashenm.ml>
2019-03-02 03:53:50 +0000

    Add :dev image

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env sh
 # Build Docker Image
 
+# :latest
 docker build --tag "$TRAVIS_REPO_SLUG" .
+
+# :dev
+docker build --tag "$TRAVIS_REPO_SLUG:dev" :dev
 
--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -6,6 +6,9 @@ set -e
 # authenticate
 echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
 
-# deploy image
+# deploy :latest
 docker push "$TRAVIS_REPO_SLUG"
+
+# deploy :dev
+docker push "$TRAVIS_REPO_SLUG:dev"
 
