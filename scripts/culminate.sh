6b80d3232956f9de8ba04513b48eba9523c0f50b
Ashen Gunaratne <mail@ashenm.ml>
2019-03-12 11:09:59 +0000

    Update reverse dependencies

--- scripts/culminate.sh
+++ scripts/culminate.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-# Trigger Dependent Builds
+# Trigger Reverse Dependency Builds
 
 set -e
 
@@ -10,12 +10,12 @@ TRAVIS_COMMIT=`expr substr "$TRAVIS_COMMIT" 1 7`
 curl --fail --silent --show-error --request POST --output /dev/null \
   --header "Content-Type: application/json" --header "Accept: application/json" \
   --header "Travis-API-Version: 3" --header "Authorization: token $TRAVIS_API_TOKEN" \
-  --data "{ \"request\": { \"branch\": \"master\", \"message\": \"Build to parity with commit "$TRAVIS_COMMIT" on "$TRAVIS_REPO_SLUG"\" } }" \
+  --data "{ \"request\": { \"branch\": \"latest\", \"message\": \"Build to parity with commit "$TRAVIS_COMMIT" on "$TRAVIS_REPO_SLUG"\" } }" \
   --url "https://api.travis-ci.org/repo/ashenm%2Fworkspace/requests"
 
 # ashenm/ide
 curl --fail --silent --show-error --request POST --output /dev/null \
   --header "Content-Type: application/json" --header "Accept: application/json" \
   --header "Travis-API-Version: 3" --header "Authorization: token $TRAVIS_API_TOKEN" \
-  --data "{ \"request\": { \"branch\": \"master\", \"message\": \"Build to parity with commit "$TRAVIS_COMMIT" on "$TRAVIS_REPO_SLUG"\" } }" \
+  --data "{ \"request\": { \"branch\": \"latest\", \"message\": \"Build to parity with commit "$TRAVIS_COMMIT" on "$TRAVIS_REPO_SLUG"\" } }" \
   --url "https://api.travis-ci.org/repo/ashenm%2Fide/requests"
