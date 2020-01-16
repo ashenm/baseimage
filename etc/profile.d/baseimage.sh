e0db7f31241b40a844fd80a33cc294531166561c
Ashen Gunaratne <mail@ashenm.ml>
2020-01-16 05:43:08 +0000

    Transfer common profile.d script excerpts to ashenm/environment

--- etc/profile.d/baseimage.sh
+++ etc/profile.d/baseimage.sh
@@ -1,42 +1,18 @@
+##################################################
+#
+# Baseimage Shell Initialisation
+# https://github.com/ashenm/baseimage
+#
+# Ashen Gunaratne
+# mail@ashenm.ml
+#
+##################################################
+
 # standard envs
 export SHELL
 
-# set editor
-export EDITOR=vim
-
 # pinentry-curses
 export GPG_TTY=`tty`
-
-# directory listing
-alias l='ls -C'
-alias la='ls -A'
-alias ll='ls -Al'
-alias ls='ls --classify --color=auto'
-
-# if not root
-if [ `id -u` -ne 0 ]
-then
-
-  # defult 700
-  umask 0077
-
-  # protect user
-  alias cp='cp -i'
-  alias mv='mv -i'
-  alias rm='rm -i'
-
-  # git
-  alias push='git push'
-  alias log='git log --oneline'
-  alias pull='git pull --rebase'
-  alias commit='git add --all; git commit'
-  alias st='git status --ignored'
-
-  # vim
-  alias vi='vim'
-  alias vim='vim -p'
-
-fi
 
 # message of the day
 clear
--- scripts/install.sh
+++ scripts/install.sh
@@ -7,6 +7,10 @@ set -e
 curl --fail --silent --show-error --create-dirs --output etc/vim/vimrc.local \
     --url https://raw.githubusercontent.com/amix/vimrc/master/vimrcs/basic.vim
 
+# environment.sh
+curl --fail --silent --show-error --create-dirs --output etc/profile.d/environment.sh \
+    --url https://raw.githubusercontent.com/ashenm/environment/master/linux/components/etc/profile.d/environment.sh
+
 # clinch permissions
 find etc -type d -exec chmod 755 {} \;
 find etc -type f -exec chmod 644 {} \;
